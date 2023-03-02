5d79ed75370dfe06981b979c8d2300c419bf88ed
Julia Ivanova <for.julia96@gmail.com>
2023-03-02 20:53:53 +0300

    Get user credentials from file

--- Authorization_books/src/main/java/com/skb/authorization_books/user/UserService.java
+++ Authorization_books/src/main/java/com/skb/authorization_books/user/UserService.java
@@ -2,20 +2,20 @@ package com.skb.authorization_books.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
   private User user = null;
 
   public UserService() {
-    user = new User("myusername", "$2a$12$Q3ucJL8PMXIZP4vOqWtIN.W6wxg/IrdINNm2fCXZekwJdQoFt1hzS", true);
+//    user = new User("myusername", "$2a$12$Q3ucJL8PMXIZP4vOqWtIN.W6wxg/IrdINNm2fCXZekwJdQoFt1hzS", true);
   }
 
   public User getUserByUsername (String username) {
-    if (user.getUsername().equals(username)) {
-      return user;
-    }
+    Optional<User> userOptional = UserRepository.findByUsername(username);
 
-    return null;
+    return userOptional.orElse(null);
   }
 }
